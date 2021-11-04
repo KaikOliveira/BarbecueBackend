c733e68f2a983c74ac5418d2e303c3348276c1cd
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-11-04 20:27:58 -0300

    startBackend

---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import express from 'express';
+
+const app = express();
+const port = 3333;
+
+app.use(express.json());
+
+app.listen(port, () => {
+  console.log(`🚩 Server started on port ${port} !`);
+});
