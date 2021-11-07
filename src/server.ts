1b3b8273258ed1ead4d8267eb47281882276efe6
KaikOliveira <kaik.oliveiraa76@gmail.com>
2021-11-07 13:57:13 -0300

    test deploy

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
+import cors from 'cors';
 import express from 'express';
 
 import { useRoutes } from './routes';
 
 const app = express();
 const port = 3333;
+
+app.use(cors());
 
 app.use(express.json());
 
